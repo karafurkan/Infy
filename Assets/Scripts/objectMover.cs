24084b4add816a70c8559c7265a494ed2a35c6fa
Furkan <furkan.kara@std.yeditepe.edu.tr>
2020-03-01 17:42:58 +0300

    Speed adjustment

--- Assets/Scripts/objectMover.cs
+++ Assets/Scripts/objectMover.cs
@@ -2,23 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//using System.Math;
+
 public class objectMover : MonoBehaviour
 {
     public static float speed = 3f;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        speed = 3f;
+        InvokeRepeating("AdjustSpeed", 0.0f, 0.2f);
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         MoveObstacle();
     }
 
-    void MoveObstacle()
-    {
+    void MoveObstacle() {
         transform.Translate(0, speed * Time.deltaTime, 0);
+    }
+
+    void AdjustSpeed() {
+                
+        if (speed < 3.5) {
+            speed = speed + 0.03f * Time.deltaTime;
+        } else if (speed < 4){
+            speed = speed + 0.04f * Time.deltaTime;
+        } else if (speed < 4.5){
+            speed = speed + 0.009f * Time.deltaTime;
+        } else if (speed < 5){
+            speed = speed + 0.0009f * Time.deltaTime;
+        } 
+
     }
 }
