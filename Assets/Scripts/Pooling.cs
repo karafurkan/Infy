0000e5e90196f422991cfc41d75a5568759d1bf9
Özgür Çebi <oz6nr@hotmail.com>
2020-03-04 17:56:20 +0300

    Initialization of obstacles
    Bugged version

--- Assets/Scripts/Pooling.cs
+++ Assets/Scripts/Pooling.cs
@@ -10,6 +10,11 @@ public class Pooling : MonoBehaviour
     public GameObject leftLastObject;
     public GameObject rightLastObject;
     public GameObject PowerUpObject;
+    public GameObject[] leftObstacleArray = new GameObject[3];
+    public GameObject[] rightObstacleArray = new GameObject[3];
+
+    public float minimumGap;
+    public float maximumGap;
 
 
     private PowerUp PowerUpControl;
@@ -27,28 +32,16 @@ public class Pooling : MonoBehaviour
         spawnPoints[1] = 2.0f;
         spawnPoints[2] = 3.0f;
 
-        //PowerUpControl = new PowerUp(); //(PowerUpObject, leftLastObject, rightLastObject);
-        
-        //leftObjectY = leftLastObject.transform.position.y;
-        //rightObjectY = rightLastObject.transform.position.y;
-        
-
-
+        InitializeObstacles();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-    
-    }
-    
 
 
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.tag == "left")
         {
-            float leftRandomInt = Random.Range(2.5f,4f);
+            float leftRandomInt = Random.Range(minimumGap,maximumGap);
             float leftY = leftLastObject.transform.position.y - leftRandomInt;
             int selection = Random.Range(0, 2);
             if (selection == 0){
@@ -66,7 +59,7 @@ public class Pooling : MonoBehaviour
         else if (col.gameObject.tag == "right")
         {
             
-            float rightRandomInt = Random.Range(2.5f,4f);
+            float rightRandomInt = Random.Range(minimumGap,maximumGap);
             float rightY = rightLastObject.transform.position.y - rightRandomInt;    
             int selection = Random.Range(0, 2);
             if (selection == 0)
@@ -88,6 +81,65 @@ public class Pooling : MonoBehaviour
             //PowerUpControl.getRandomPosition();
         }
 
+    }
+
+    private void InitializeObstacles()
+    {
+        //initialize left obstacles
+        int selection = Random.Range(0, 2);
+        if (selection == 0)
+        {
+            leftLastObject.transform.position = new Vector3(-2.1f, -2, 0f);
+        }
+        else
+        {
+            leftLastObject.transform.position = new Vector3(-0.67f, -2, 0f);
+        }
+        
+        foreach (GameObject go in leftObstacleArray)
+        {
+            Debug.Log("left: " + go.name);
+            float leftRandomInt = Random.Range(minimumGap, maximumGap);
+            float leftY = leftLastObject.transform.position.y - leftRandomInt;
+            selection = Random.Range(0, 2);
+            if (selection == 0)
+            {
+                go.transform.position = new Vector3(-2.1f, leftY, 0f);
+            }
+            else
+            {
+                go.transform.position = new Vector3(-0.67f, leftY, 0f);
+            }
+            leftLastObject = go;
+        }
+
+        //initialize right obstacles
+        selection = Random.Range(0, 2);
+        if (selection == 0)
+        {
+            rightLastObject.transform.position = new Vector3(0.59f, -2, 0f);
+        }
+        else
+        {
+            rightLastObject.transform.position = new Vector3(2.24f, -2, 0f);
+        }
+
+        foreach (GameObject go in leftObstacleArray)
+        {
+            Debug.Log("right: "+ go.name);
+            float rightRandomInt = Random.Range(minimumGap, maximumGap);
+            float rightY = rightLastObject.transform.position.y - rightRandomInt;
+            selection = Random.Range(0, 2);
+            if (selection == 0)
+            {
+                go.transform.position = new Vector3(0.59f, rightY, 0f);
+            }
+            else
+            {
+                go.transform.position = new Vector3(2.24f, rightY, 0f);
+            }
+            rightLastObject = go;
+        }
     }
 
 }
